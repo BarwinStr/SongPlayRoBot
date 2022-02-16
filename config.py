a2a54d316c747d72e07fa4119a607852979a7ae8
BarwinStr <99816552+BarwinStr@users.noreply.github.com>
2022-02-16 20:36:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "9253140"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "fa5b41a7d8ef7d28446c2173c8dfd023" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5195956472:AAHwk0QmH-jLuAji0vQWN9DkBFRFBzfukos" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
